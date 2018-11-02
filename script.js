723b284b14677f53c033488482b01206ebb91905
Quang Đặng Anh <quangdtvtdhv@gmail.com>
2018-11-02 20:55:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myScript(){
+    var cao = parseInt(prompt("Bạn nhập chiều cao"));
+    var rong = parseInt(prompt("Bạn nhập chiều rộng"));
+    var DT = cao * rong;
+    document.write("Diện tích = " + DT);
+
+}
